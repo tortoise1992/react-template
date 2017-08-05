4d4820fce439740ec2bad4caf2029d42a62fab4f
ahui666 <772084015@qq.com>
2017-08-05 14:01:41 +0800

    react模板v1.0.0

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {render} from 'react-dom'
+
+
+class App extends Component{
+    render(){
+        return(
+            <div>hello</div>
+        )
+    }
+}
+
+
+
+render(
+    <App/>,
+    document.getElementById('app')
+)
